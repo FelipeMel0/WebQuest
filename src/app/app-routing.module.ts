1cf74fc45ef772786e91f7032a97b73c98e7c029
Felipe de Melo Fernandes <79909903+FelipeMel0@users.noreply.github.com>
2024-03-19 22:35:06 -0300

    feat: Primeiro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
   }
 ];
 @NgModule({
